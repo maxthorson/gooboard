9a71928d22c5edd19838f9fba55ebdd922c3a7e8
max <maxthorson1@gmail.com>
2025-07-13 13:39:27 -0400

    Initial Commit

---
+++ Gooboard/GooboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GooboardApp.swift
+//  Gooboard
+//
+//  Created by Max Thorson on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GooboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GooboardTests/GooboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  GooboardTests.swift
+//  GooboardTests
+//
+//  Created by Max Thorson on 7/12/25.
+//
+
+import Testing
+@testable import Gooboard
+
+struct GooboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
